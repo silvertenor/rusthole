5d603476b4127b1bddc24e3a6661d715689da13b
Devin Lane <silvertenor_dev@protonmail.com>
2025-06-12 23:37:33 -0400

    Working

--- dnsCacher/src/main.rs
+++ dnsCacher/src/main.rs
@@ -1,6 +1,6 @@
 use dns_lookup::lookup_host;
 use std::io::Result;
-use std::net::UdpSocket;
+use std::net::{SocketAddr, UdpSocket};
 use std::{collections::HashMap, fs::read_to_string, net::IpAddr};
 pub mod packet;
 use crate::packet::{DnsPacket, Header, ParsedSection, Query, Record, Section};
@@ -33,13 +33,26 @@ fn lookup_hostname(hostname: &String) -> Result<Vec<IpAddr>> {
     Ok(ips)
 }
 
-fn handle_client(message_buf: Vec<u8>, dns_records: &HashMap<String, Vec<IpAddr>>) -> Vec<u8> {
-    println!("{:?}", message_buf);
+#[derive(Debug)]
+struct ReturnType {
+    id: String,
+    response: Option<Vec<u8>>,
+}
+fn handle_client(
+    socket: &UdpSocket,
+    message_buf: Vec<u8>,
+    dns_records: &HashMap<String, Vec<IpAddr>>,
+) -> ReturnType {
+    let mut return_type = ReturnType {
+        id: String::new(),
+        response: None,
+    };
     let mut dns_packet = DnsPacket::new(&message_buf);
     // Extract header from buffer
     let h = handle_section(Section::Header, &message_buf, &mut dns_packet);
     // If header is DNS query, parse the queries - if not, forward the packet
     if let ParsedSection::Header(mut header) = h {
+        return_type.id = header.id.to_string();
         // If packet is a DNS query:
         if !header.response {
             println!("Incoming query!");
@@ -56,47 +69,65 @@ fn handle_client(message_buf: Vec<u8>, dns_records: &HashMap<String, Vec<IpAddr>
                     dns_packet.set_query(&question);
                     let r = Record::new(&question, dns_records);
                     dns_packet.set_answer(&r);
+                    return_type.response = Some(dns_packet.build_packet());
                 } else {
                     println!("Query not in block list. Forwarding to gateway");
-                    // Clear buffer:
-                    let mut buf: [u8; 512] = [0; 512];
-                    // Bind to ephemeral port:
-                    let forwarding_socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind");
-                    // if query is not in blocked list, forward the packet to the "real" DNS server
-                    forwarding_socket
+                    socket
                         .send_to(&message_buf, "192.168.50.1:53")
                         .expect("error");
-                    let (number_of_bytes, _) = forwarding_socket
-                        .recv_from(&mut buf)
-                        .expect("Didn't receive data");
-                    let filled_buf = Vec::from(buf.get(..number_of_bytes).unwrap());
-                    return filled_buf;
-                }
+                };
             }
+        } else {
+            // if message is response
+            return_type.response = Some(message_buf);
         }
     }
-    dns_packet.build_packet()
+    return_type
 }
 
 fn main() -> Result<()> {
-    let lines: Vec<String> = get_hostnames_to_block("blockList.conf");
+    let lines: Vec<String> = get_hostnames_to_block("blockList.conf.prod");
     let mut dns_records = HashMap::new();
+    let mut count = 0;
     for line in lines {
         if let Ok(ips) = lookup_hostname(&line) {
+            println!("{}", count);
+            count += 1;
             dns_records.insert(line, ips);
         };
     }
 
     let socket = UdpSocket::bind("0.0.0.0:53")?;
     println!("DNS server running on 0.0.0.0:53");
-
+    let mut clients: HashMap<String, SocketAddr> = HashMap::new(); // DNS QUERY ID: IP ADDRESS THAT REQUESTED IT
     loop {
+        println!("{:?}", clients);
         // DNS packets are limited to 512 bytes
         let mut buf = [0; 512];
         let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data");
         let filled_buf = Vec::from(buf.get(..number_of_bytes).unwrap());
-        let result = handle_client(filled_buf, &dns_records);
-        println!("Result: {:?}", result);
-        socket.send_to(&result, src_addr).expect("error");
+        let result = handle_client(&socket, filled_buf, &dns_records);
+        match result.response {
+            Some(r) => {
+                println!("Response received for ID: {:?}", result.id);
+                if clients.contains_key(&result.id) {
+                    println!(
+                        "Response is for previous entry in hash map. Sending to {:?}",
+                        &clients.get(&result.id).unwrap()
+                    );
+                    socket.send_to(&r, &clients.get(&result.id).unwrap())?;
+                    clients.remove_entry(&result.id);
+                } else {
+                    socket.send_to(&r, src_addr)?;
+                }
+            }
+            None => {
+                println!(
+                    "No response received. Inserting ID {} into hash map with source address {:?}",
+                    result.id, src_addr
+                );
+                clients.insert(result.id, src_addr);
+            }
+        }
     }
 }
